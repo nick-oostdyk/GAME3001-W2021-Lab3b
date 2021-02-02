a2e64f5e8500f75fc04a5059da8a1b72b1a53522
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-02-02 16:34:12 -0500

    cleaned Play Scene

--- src/PlayScene.h
+++ src/PlayScene.h
@@ -26,14 +26,9 @@ private:
 	// IMGUI Function
 	void GUI_Function() const;
 	std::string m_guiTitle;
-	
 	glm::vec2 m_mousePosition;
 
-	SpaceShip* m_pSpaceShip;
 	Target* m_pTarget;
-	Obstacle* m_pObstacle;
-	
-	// UI Items
 	
 };
 
